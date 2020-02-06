85872e451188cbab824cd85d38fe8e55d9151616
Thomas Schlosser <thomas@schlosser-ma.de>
2020-02-06 16:02:50 +0100

    NEW: posting external HTML tableau with every run

--- minelli.py
+++ minelli.py
@@ -15,8 +15,10 @@ input = '''Ha P W T&R
 rounds = input.split('\n')
 moneybalance = [0,0,0,0]
 chipbalance = [0,0,0,0]
+htmlboard = '</table></html>'
 	
 def evaluate(game,type):
+	global htmlboard
 	gameresult = []
 	for c in game:
 		gameresult.append(int(c)) 
@@ -55,20 +57,35 @@ def evaluate(game,type):
 			
 	if (type==4 and checksum!=32) or (type in (0,1,2,3) and checksum!=-8) or not (type in range(5)): print("**ERROR**")
 	
+	#print (htmlboard)
+	#htmlboard = htmlboard + 'x'
+	htmlboard = '<tr><td>'+str(type+1)+'</td><td>'+game+'</td><td>'+str(moneybalance[0])+'</td><td>'+str(moneybalance[1])+'</td><td>'+str(moneybalance[2])+'</td><td>'+str(moneybalance[3])+'</td></tr>' + htmlboard
+	
 	resultstr = '{0:3d}|{1:3d}|{2:3d}|{3:3d}'.format(moneybalance[0], moneybalance[1],moneybalance[2],moneybalance[3])
 	resultstr = resultstr + '   ({0:2d}|{1:2d}|{2:2d}|{3:2d}) '.format(gameresult[0], gameresult[1],gameresult[2],gameresult[3])
 	resultstr = resultstr + '   {0:3d}|{1:3d}|{2:3d}|{3:3d} '.format(chipbalance[0], chipbalance[1],chipbalance[2],chipbalance[3])
 	resultstr = resultstr + '  {0:3d}|{1:3d}|{2:3d}|{3:3d} '.format(eurobalance[0], eurobalance[1],eurobalance[2],eurobalance[3])
-	return (resultstr) #(str(gameresult) + ">" + str(moneybalance) + "|" + str(chips) + ">" + str(chipbalance) + "€" + str(eurobalance))
+	return (resultstr) 
+	#(str(gameresult) + ">" + str(moneybalance) + "|" + str(chips) + ">" + str(chipbalance) + "€" + str(eurobalance))
 
 
 for roundidx,round in enumerate(rounds):
 	games = round.split(' ')
 	
 	if roundidx>0 and len(games)>0:
-		print (rounds[0] + "       PUNKTE TOT.         Punkte           Chips-tot          € tot.")
+		print (rounds[0] + "        PUNKTE TOT.        Punkte           Chips-tot          € tot.")
 		for idx,game in enumerate(games):
 			dealer = 1 + (roundidx*5+idx-1) % 4
 			resultstring = evaluate(game,idx)
 			print("S%2d/%1d[%1d]: %4s %s" % (roundidx,idx+1,dealer, game,resultstring))
 			if idx==4: print()
+			
+#htmlboard = 'Spielstand:<table><th><td>1</td><td>2</td><td><td>3</td><td>4</td></th><tr><td>'+str(moneybalance[0])+'</td><td>'+str(moneybalance[1])+'</td><td>'+str(moneybalance[2])+'</td><td>'+str(moneybalance[3])+'</td></tr>'
+htmlboard= '<table border="1"><tr><th align="center">Art</th><th align="center">Ergebnis</th><th>1</th><th>2</th><th>3</th><th>4</th></tr>' + htmlboard
+htmlboard = '<html><body><style>td {border: 1px solid; padding: 8px; text-align: right;} body {width: 100%; }</style>' + htmlboard
+
+import requests
+url = 'http://net-hitec.de/mvonline/minelliupload.php'
+files = {'userfile': ('minelli.html', htmlboard)}
+r = requests.post(url, files=files)
+print (r.text)
